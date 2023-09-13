bd724230c80e1172c85a25f353194b9cb20e7be3
Donya <donya.asayesh@outlook.com>
2023-09-13 17:39:48 +0330

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Center(
+    child: Text(
+      "Hello , its me. \n this is my first app :) \n cheshmeton darad hasodaye pelastiki :D \n montazere appe khafanemoon bashid ;)",
+      textAlign: TextAlign.center,
+      textDirection: TextDirection.ltr,
+      style: TextStyle(color: Color.fromARGB(255, 255, 123, 0)),
+    ),
+  ));
+}
